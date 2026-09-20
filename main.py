fb221d13eed69c0f0ba01df510e8a9c277eaa761
west coder <furius1476@gmail.com>
2026-09-20 19:40:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ load_dotenv()
 from database import init_pool, close_pool
 from runtime import bot, dp
 from parser.client import ParserClient
+from parser.dispatcher import broadcast_worker
 
 logging.basicConfig(
     level=logging.INFO,
@@ -29,6 +30,8 @@ async def main():
 
     parser = ParserClient(bot)
     asyncio.create_task(parser.start())
+
+    asyncio.create_task(broadcast_worker(bot, interval=300))
 
     try:
         await dp.start_polling(bot)
